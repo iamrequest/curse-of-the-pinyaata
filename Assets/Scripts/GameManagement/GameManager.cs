6caa34a587bf6d5fc970bcb038b08aacc3010783
request <iamrequest@protonmail.com>
2022-02-20 15:22:03 -0500

    Added base game manager, base score manager
    Added the base framework for playing a timed game, plus the score manager
    Next steps:
    	- Add an event channel for game manager
    	- Optional: Add more details to high score (player name, timestamp, maybe record a ghost like in mario kart?)

---
+++ Assets/Scripts/GameManagement/GameManager.cs
@@ -0,0 +1,71 @@
+﻿using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState {
+    STOPPED, ACTIVE
+}
+public class GameManager : MonoBehaviour {
+    public GameState gameState { get; private set; }
+    public float gameDurationTotal, gameDurationCurrent;
+
+    private Coroutine gameTimerCoroutine;
+
+    public static GameManager Instance { get; private set; }
+
+    private void Awake() {
+        if (Instance == null) {
+            Instance = this;
+        } else {
+            Debug.LogError("Multiple GameManager components detected. This is probably a bug.");
+            Destroy(this);
+        }
+    }
+
+    [Button]
+    [ButtonGroup("Game Start Stop")]
+    public void StartGame() {
+        if (gameState == GameState.ACTIVE) {
+            return;
+        }
+
+        // Start the game, and game timer
+        gameState = GameState.ACTIVE;
+        gameTimerCoroutine = StartCoroutine(EndGameAfterDelay());
+
+        // Reset the score
+        ScoreManager.Instance.ResetScore();
+    }
+
+    [Button]
+    [ButtonGroup("Game Start Stop")]
+    public void EndGame() {
+        if (gameState != GameState.ACTIVE) {
+            return;
+        }
+
+        gameState = GameState.STOPPED;
+
+        // Stop the game timer
+        if (gameTimerCoroutine != null) {
+            StopCoroutine(gameTimerCoroutine);
+            gameTimerCoroutine = null;
+        }
+
+        // Attempt to add this score to the high score board
+        ScoreManager.Instance.CompareScoreToHighScore();
+    }
+    private IEnumerator EndGameAfterDelay() {
+        gameDurationCurrent = 0f;
+
+        // Wait until the game is over
+        while (gameDurationCurrent < gameDurationTotal) {
+            gameDurationCurrent += Time.deltaTime;
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+
+        // Optional: Snap the elapsed time to max time here
+        EndGame();
+    }
+}
---
+++ Assets/Scripts/GameManagement/ScoreManager.cs
@@ -0,0 +1,60 @@
+﻿using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour {
+    public int currentScore;
+    // TODO: Depending on time constraints & UI options, consider making this a separate object with player name and timestamp
+    public int maxNumHighScores = 10;
+    public List<int> highScores;
+
+    public static ScoreManager Instance { get; private set; }
+
+    private void Awake() {
+        if (Instance == null) {
+            Instance = this;
+        } else {
+            Debug.LogError("Multiple ScoreManager components detected. This is probably a bug.");
+            Destroy(this);
+        }
+    }
+
+    private void Start() {
+        // Init high scores list
+        for (int i = 0; i < maxNumHighScores; i++) { highScores.Add(0); }
+    }
+
+    public void AddScore(int addedScore) {
+        if (GameManager.Instance.gameState == GameState.ACTIVE) {
+            currentScore += addedScore;
+        }
+    }
+
+    [Button("Add 10 points")]
+    [ButtonGroup("Debug")]
+    private void DebugAddScore() {
+        AddScore(10);
+    }
+
+    [Button]
+    [ButtonGroup("Debug")]
+    public void ResetScore() {
+        currentScore = 0;
+    }
+
+    /// <summary>
+    /// Given currentScore, add it to the list of high scores if it beats any of the current scores
+    /// </summary>
+    [Button]
+    [ButtonGroup("Debug")]
+    public void CompareScoreToHighScore() {
+        for(int i = 0; i < maxNumHighScores; i++) {
+            if (currentScore > highScores[i]) {
+                highScores.RemoveAt(highScores.Count - 1);
+                highScores.Insert(i, currentScore);
+                return;
+            }
+        }
+    }
+}
