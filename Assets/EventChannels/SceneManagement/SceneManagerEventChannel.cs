89140a957253ce7604b1240c5706ecd330e5fc31
request <iamrequest@protonmail.com>
2022-02-20 21:43:17 -0500

    Created scene manager event channel
    - Created prefab to render high scores, need to hook it up next

---
+++ Assets/EventChannels/SceneManagement/SceneManagerEventChannel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Usage: Invoke RaiseEvent() whenever we need to switch scenes
+/// https://www.youtube.com/watch?v=WLDgtRNK2VE
+/// </summary>
+[CreateAssetMenu(menuName = "Events/Scene Manager Event Channel")]
+public class SceneManagerEventChannel : ScriptableObject {
+    // These events are responsible for invoking an action
+    public UnityAction<string> beforeSceneLoad;
+
+    public void LoadScene(string sceneName) {
+        // Make sure that this scene exists in build settings
+        if (SceneManager.GetSceneByName(sceneName) == null) {
+            Debug.LogError("Attempted to load a scene, but no scene with that name exists in build settings");
+            return;
+        }
+
+        // Alert downstream scripts, if necessary
+        if (beforeSceneLoad != null) {
+            beforeSceneLoad.Invoke(sceneName);
+        }
+
+        // Load the scene
+        SceneManager.LoadScene(sceneName);
+    }
+}
