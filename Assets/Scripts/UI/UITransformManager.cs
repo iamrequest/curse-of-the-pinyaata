7f10b0d46391b2c9324994c757ef80d9f4e8c0e8
request <iamrequest@protonmail.com>
2022-02-25 18:06:42 -0500

    Dialog UI now looks at player

---
+++ Assets/Scripts/UI/UITransformManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Aligns world UI to optionally follow a position transform, and/or look at the player's camera
+public class UITransformManager : MonoBehaviour {
+    public Transform targetPositionTransform;
+
+    public bool alignPositionToTarget;
+    public bool lookatPlayer;
+
+    private void Update() {
+        if (alignPositionToTarget) AlignPosition();
+        if (lookatPlayer) LookAtPlayer();
+    }
+
+    public void AlignPosition() {
+        if(targetPositionTransform) transform.position = targetPositionTransform.position;
+    }
+
+    public void LookAtPlayer() {
+        transform.forward = Vector3.ProjectOnPlane(Player.Instance.playerController.Camera.position - transform.position, Vector3.up).normalized;
+    }
+}
