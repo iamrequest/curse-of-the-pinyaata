b7363c5fbf02e086148643571cb45f58c5a47d9f
request <iamrequest@protonmail.com>
2022-02-24 23:26:32 -0500

    Added timer/score UI to player hand

---
+++ Assets/Scripts/UI/HandUIVisibilityManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Freya;
+
+// Lerp alpha of UI elements based on viewing angle
+public class HandUIVisibilityManager : MonoBehaviour {
+    public Image backgroundImage;
+    public TextMeshProUGUI textField;
+    [Range(0f, 180f)]
+    public float openAngle = 30f, closedAngle = 45f;
+
+    private void Update() {
+        UpdateOpacity();
+    }
+
+    private void UpdateOpacity() {
+        float viewAngle = Vector3.Angle(Player.Instance.playerController.Camera.forward, transform.forward);
+        float opacity = Mathfs.RemapClamped(closedAngle, openAngle, 0f, 1f, viewAngle);
+
+        // Set background image alpha
+        Color color = backgroundImage.color;
+        color.a = opacity;
+        backgroundImage.color = color;
+
+        // Set text field alpha
+        // TODO: This doesn't seem to work, but the text is dark enough that maybe it's not noticeable
+        color = textField.faceColor;
+        color.a = opacity;
+        textField.faceColor = color;
+    }
+
+    private void RenderAngleToTextField() {
+        float viewAngle = Vector3.Angle(Player.Instance.playerController.Camera.forward, transform.forward);
+        textField.text = viewAngle.ToString("000.0");
+    }
+
+    private void RenderOpacityToTextField() {
+        float viewAngle = Vector3.Angle(Player.Instance.playerController.Camera.forward, transform.forward);
+        float opacity = Mathfs.RemapClamped(closedAngle, openAngle, 0, 255, viewAngle);
+        textField.text = opacity.ToString("000.0");
+    }
+}
