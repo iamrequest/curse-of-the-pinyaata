a8636488210ff1ddd0060fd0bbcea545a0126c33
request <iamrequest@protonmail.com>
2022-02-25 19:31:01 -0500

    More progress on real world first floor

--- Assets/Scripts/UI/UITransformManager.cs
+++ Assets/Scripts/UI/UITransformManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // Aligns world UI to optionally follow a position transform, and/or look at the player's camera
+// TODO: Lerp instead of sudden movement, to smooth movement
 public class UITransformManager : MonoBehaviour {
     public Transform targetPositionTransform;
 
