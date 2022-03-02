f5c3a14611671eafeadb854fb86f6a37ce075600
request <iamrequest@protonmail.com>
2022-03-01 22:47:04 -0500

    Added scene to record gif for itch page, desktop cam

---
+++ Assets/Scripts/Screenshot/DesktopCamera.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HurricaneVR.Framework.ControllerInput;
+
+[RequireComponent(typeof(Camera))]
+public class DesktopCamera : MonoBehaviour {
+    private Camera cam;
+    public float depth;
+
+    [Range(0f, 1f)]
+    public float positionSpeed, rotationSpeed;
+
+    private void Start() {
+        cam = GetComponent<Camera>();
+        cam.depth = depth;
+    }
+
+    private void Update() {
+        transform.position = Vector3.Slerp(transform.position, Player.Instance.playerController.Camera.position, positionSpeed);
+        transform.rotation = Quaternion.Slerp(transform.rotation, Player.Instance.playerController.Camera.rotation, rotationSpeed);
+
+        if (HVRGlobalInputs.Instance.RightTriggerButtonState.JustActivated) {
+            Screenshot();
+        }
+    }
+
+    private void Screenshot() {
+        DateTime dt = DateTime.Now;
+        String filename = $"{dt.Year}-{dt.Month}-{dt.Day}_{dt.Hour}-{dt.Minute}-{dt.Second}-{dt.Millisecond}.png";
+        Debug.Log($"Saving screenshot as ./{filename}");
+        ScreenCapture.CaptureScreenshot(filename, 1);
+    }
+} 
---
+++ Assets/Scripts/Screenshot/ScreenshotPingShaderManagerSingle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Same thing as the original renderer, but this only manages a single ping sweep - useful for animators
+public class ScreenshotPingShaderManagerSingle : MonoBehaviour {
+    public Material mat;
+    public Color baseColor;
+    public List<Transform> points;
+    public float distance;
+    public Color colors;
+
+    private void OnDrawGizmos() {
+        if (!mat) return;
+
+        mat.SetColor("baseColor", baseColor);
+
+        Vector4 posSource;
+        for (int i = 0; i < points.Count; i++) {
+            posSource = new Vector4(points[i].position.x, points[i].position.y, points[i].position.z);
+            posSource.w = distance;
+
+            mat.SetVector($"posSource_0", posSource);
+            mat.SetColor($"color_0", colors);
+        }
+    }
+}
